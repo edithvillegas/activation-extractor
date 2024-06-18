d9a91277fc9f7f594551ab0aca2b5fb952679fa2
Edith Villegas <29732178+edithvillegas@users.noreply.github.com>
2024-06-18 16:43:36 +0200

    Update model_types.py documentation string

--- src/activation_extractor/model_functions/model_types.py
+++ src/activation_extractor/model_functions/model_types.py
@@ -1,4 +1,7 @@
-# Note: large models have to be downloaded first wit git LFS!
+"""
+This file contains a list of ``model types`` supported by default.
+Note: large models have to be downloaded first wit git LFS!
+"""
 
 model_types = {  
     "sequence": 
