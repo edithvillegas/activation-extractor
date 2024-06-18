81b27701cca774ad1541792637051b39b29a85c4
Edith Villegas <29732178+edithvillegas@users.noreply.github.com>
2024-06-18 17:25:52 +0200

    Update intermediateExtractor.py docstring and moved model specific functions

--- src/activation_extractor/extractors/intermediateExtractor.py
+++ src/activation_extractor/extractors/intermediateExtractor.py
@@ -3,26 +3,34 @@ import os, shutil
 from collections import OrderedDict
 
 import torch
-from transformers.modeling_outputs import (MaskedLMOutput, 
-                                            BaseModelOutputWithNoAttention, 
-                                            BaseModelOutputWithPastAndCrossAttentions)
 
 from activation_extractor.extractors.intermediateExtractorBase import IntermediateExtractorBase
+from activation_extractor.model_functions.embedding_to_numpy import embedding_to_numpy 
 
-# Class ====================================================================================
 class IntermediateExtractor(IntermediateExtractorBase):
+  """
+  Extends the functionality of the ``IntermediateExtractorBase`` to automatically save activations.
+  """
+  
     #Save outputs ---------------------------------------------------------------------------
     def gpu_to_cpu(self):
+        """
+        Takes all the intermediate activations stored in the extractor object and moves them to CPU
+        (if on GPU) after formatting them to a numpy array.
+        """
         for name in self.layer_list:
             self.intermediate_outputs[name] = embeddings_to_numpy(self.intermediate_outputs[name])  
 
-    def save_outputs(self, output_folder, reset=False, move_to_cpu=True, saving_type="numpy_compressed", emb_format='full'):
-        '''
+    def save_outputs(self, output_folder, reset=False, move_to_cpu=True, 
+                     saving_type="numpy_compressed", emb_format='full'):
+        """
         Save intermediate activation dictionary to output folder.
         You can choose:
-        - the saving function (numpy_compressed or numpy) 
-        - the embedding format (full, mean, LT: last token).
-        '''
+        
+        * the saving function (numpy_compressed or numpy) 
+        * the embedding format (full, mean, LT: last token).
+        """
+      
         #check the format
         if emb_format not in ['full','mean','LT']:
             raise ValueError(f"format must be 'full','mean' or 'LT'; got {emb_format} instead.")
@@ -51,45 +59,3 @@ class IntermediateExtractor(IntermediateExtractorBase):
                 case "numpy":
                     np.save(f'{output_folder}/{name}.npy',
                                 outputs)
-
-#Functions ==============================================================================
-def embedding_to_numpy(embeddings):
-    """
-    Converts different types of module outputs to a numpy array.
-    Handles different cases for the different models.
-    Additionally, moves from GPU to CPU. 
-
-    :param embedding: Intermediate output object from a pytorch model layer/module.
-    
-    :return: intermediate output as a numpy array
-    :rtype: numpy array
-    """
-    
-    #convert list to array (for protT5 tokens)
-    if isinstance(embeddings, list): 
-        embeddings = np.array(embeddings) 
-        return embeddings
-    
-    #convert output object to tensor -------------------------
-    #intermediate activations
-    if isinstance(embeddings, tuple): 
-        embeddings = embeddings[0] 
-        
-    #NT/ESM output object
-    if isinstance(embeddings, MaskedLMOutput): 
-        embeddings = embeddings['logits'] 
-        
-    #HYENA or ANKH output object
-    if (isinstance(embeddings, BaseModelOutputWithNoAttention) 
-        or isinstance(embeddings, BaseModelOutputWithPastAndCrossAttentions)):
-        embeddings = embeddings['last_hidden_state']  
-    #convert output object to tensor -------------------------
-    
-    #float conversions for tensors 
-    if torch.is_tensor(embeddings):
-        if embeddings.dtype==torch.bfloat16: embeddings = embeddings.float() 
-   
-    #to CPU numpy array
-    embeddings = embeddings.cpu().detach().numpy()   
-    
-    return embeddings
