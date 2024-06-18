610bfabe8f96dd3f60c16ee1c604dacf9635e23b
Edith Villegas <29732178+edithvillegas@users.noreply.github.com>
2024-06-18 16:46:17 +0200

    Update documentation and remove no grad

--- src/activation_extractor/model_functions/inference_funs.py
+++ src/activation_extractor/model_functions/inference_funs.py
@@ -4,6 +4,7 @@ def define_inference_function(model_type, model, tokenizer, device):
     """
     Define the right function to do inference based on the model type.
     The resulting function is called as inferencer.inference().
+    The functions move the tokenized input to ``device`` before performing inference.
 
     :param model_type: the model type (from the list in activation_extractor.model_functions.model_types)
     :type model_type: str
@@ -19,7 +20,6 @@ def define_inference_function(model_type, model, tokenizer, device):
         #🥩,🧬 
         case "esm" | "nucleotide-transformer" :  
             #### start function definition
-            @torch.no_grad()
             def inference_fun(tokenized_inputs, device, **kwargs):
 
                 tokens_ids = tokenized_inputs["input_ids"].to(device)
@@ -45,7 +45,6 @@ def define_inference_function(model_type, model, tokenizer, device):
         #🥩, 🥩
         case "prot_t5" | "ankh" :
             #### start function definition
-            @torch.no_grad()
             def inference_fun(tokenized_inputs, device, **kwargs):
                 tokens_ids = torch.tensor(tokenized_inputs['input_ids']).to(device)
                 attention_mask = torch.tensor(tokenized['attention_mask']).to(device)
@@ -59,7 +58,6 @@ def define_inference_function(model_type, model, tokenizer, device):
         #🧬, 🧬
         case "hyenadna" | "evo" | "caduceus" :
             #### start function definition
-            @torch.no_grad()
             def inference_fun(tokenized_inputs, device, **kwargs):
                 tokens_ids = tokenized_inputs["input_ids"].to(device)
                 outputs = model(tokens_ids)
